221d8da8ae80af39dcf295348a41835688051109
Avichai Israeli <57321080+avichai1221@users.noreply.github.com>
2020-11-26 12:28:05 +0200

    finish login activity

--- app/src/main/java/com/example/rommies/Login.java
+++ app/src/main/java/com/example/rommies/Login.java
@@ -2,7 +2,9 @@ package com.example.rommies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -37,10 +39,16 @@ public class Login extends AppCompatActivity {
                     return;
                 }
                 Toast.makeText(Login.this, "login succeed", Toast.LENGTH_SHORT).show();
-
-
+                Intent intent=new Intent(this,afterRegister.class);
+                startActivity(intent);
             });
 
         });
     }
+
+
+
+
+
+
 }
